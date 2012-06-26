09cabf909814501c58986dc576ef9c976c5c545b
Rotaru Vlad <vlad.marius.rom@gmail.com>
2012-06-26 16:15:02 +0300

    -- a frame class to handel time based frames -- takes in to consideration code time and shortens frame duration accordingly

---
+++ cuzzea/js/classes/core/Frame.js
@@ -0,0 +1,35 @@
+var Frame = function(){
+	var listners={},
+		timer,
+		can_run,
+		ms=SETTINGS.game.frame_duration,
+		step=function(){
+			if(!can_run) return ;
+			fireEvent(Frame.ON_ENTER);
+			timer = setTimeout(step.bind(this),ms);
+			fireEvent(Frame.ON_EXIT);
+		},
+		pause=function(){
+			can_run = false;
+			clearTimeout(timer);
+		},
+		start=function(){
+			can_run = true;
+			step();
+		},
+		fireEvent=function(type){
+			if(!listners[type]) return ;
+			for(var object in listners[type]) listners[type][object].call(object);
+		};
+	this.addListner = function(object,type,cbk){
+		if(!listners[type]) listners[type] = {};
+		listners[type][object]=cbk;
+	};
+	this.removeListner = function(object,type,cbk){
+		delete listners[type][object];
+	};
+	this.pause = function(){pause()};
+	start();
+}
+Frame.ON_ENTER = "on_enter";
+Frame.ON_EXIT  = "on_exit";

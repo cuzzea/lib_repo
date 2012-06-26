1f187d3195df45a1be630720938463c25ec4b7f4
Rotaru Vlad <vlad.marius.rom@gmail.com>
2012-06-26 16:44:52 +0300

    -- changed options to object

--- cuzzea/js/functions/core/async_for.js
+++ cuzzea/js/functions/core/async_for.js
@@ -1,42 +1,46 @@
 /*
  * params:
  * 	object: 		the array to parse
- *	cbk:			function to call whenwhen object is found in array
- *						params: i,object[i]
- *	limit_calback:	function to call when limit is reached
- *						params: i, object_length
- *	end_cbk:		function to call when loop is finished
- *						params: none
- *	limit:			number of iteration before breacking the for loop
- *						default: object.length/100
- *	timeout:		time until start of the for loop(ms)
- *						default: 1
- *	start:			the index from where to start the for loop
- *						default: 0
+ *	settings_object:
+ *		cbk:			function to call whenwhen object is found in array
+ *							params: i,object[i]
+ *		limit_calback:	function to call when limit is reached
+ *							params: i, object_length
+ *		end_cbk:		function to call when loop is finished
+ *							params: none
+ *		limit:			number of iteration before breacking the for loop
+ *							default: object.length/100
+ *		timeout:		time until start of the for loop(ms)
+ *							default: 1
+ *		start:			the index from where to start the for loop
+ *							default: 0
  * exemple:
  * 
  * var a = [];
  * a.length = 1000;
- * async_for_each(a,function(){},function(i,l){console.log("locading %s/%s - %s%",i,l,Math.round(i*100/l))});
+ * async_for_each(a,{
+ *	limit_callback:function(i,l){console.log("loading %s/%s - %s%",i,l,Math.round(i*100/l))}
+ * });
  *
  */
 
-function async_for_each(object,cbk,limit_callback,end_cbk,limit,timeout,start){
+function async_for_each(object,settings){
     var l=object.length;
-	limit = limit || Math.round(l/100);
-	start = start || 0;
-	timeout = timeout || 1;
-	for(var i=start;i<l;i++){
-		if(i-start>=limit){
+	settings.limit = settings.limit || Math.round(l/100);
+	settings.start = settings.start || 0;
+	settings.timeout = settings.timeout || 1;
+	for(var i=settings.start;i<l;i++){
+		if(i-settings.start>=settings.limit){
 			setTimeout(function(){
-				async_for_each(object,cbk,limit_callback,end_cbk,limit,timeout,i)
-			},timeout);
-			limit_callback(i,l);
+				settings.start = i;
+				async_for_each(object,settings)
+			},settings.timeout);
+			settings.limit_callback ? settings.limit_callback(i,l) : null;
 			return false;
 		}else{
-			cbk(i,object[i]);
+			settings.cbk ? settings.cbk(i,object[i]) : null;
 		}
 	}
-	end_cbk?end_cbk():null;
+	settings.end_cbk?settings.end_cbk():null;
 	return true;
 }
